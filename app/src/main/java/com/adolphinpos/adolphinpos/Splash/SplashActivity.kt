97b1edfdeec79134b0cdc90e25e6a18bfdc1a397
walaaAlshobaki <walaaalshobaki956@gmail.com>
2020-12-28 00:06:18 +0200

    test commit

---
+++ app/src/main/java/com/adolphinpos/adolphinpos/Splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.adolphinpos.adolphinpos.Splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.adolphinpos.adolphinpos.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
